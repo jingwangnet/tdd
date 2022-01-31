0c48c25e799b18d8932b37a6f12b077bc87bdcfb
jing Wang <admin@jingwangnet.com>
2022-01-31 20:36:39 +0800

    Add new user story: test layout and styling.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -113,6 +113,34 @@ class NewVisitorTest(LiveServerTestCase):
 
         
         
+    def test_layout_and_stlying(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 756)
+
+        # she notices the inputbox  is necely centered
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, 
+            delta=10
+        )
+
+        # she types something into text box
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_check_text_in_the_rows('1: Buy milk')
+
+        # There is still a centered inputbox
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, 
+            delta=10
+        )
 
 
 
