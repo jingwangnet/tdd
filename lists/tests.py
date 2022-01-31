55e4c012097cdc5480f6b497751259f969c3b0c1
jing Wang <admin@jingwangnet.com>
2022-01-31 09:55:01 +0800

    Bit the bullet: Adujuting our models.

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from .views import home_page
-from .models import Item
+from .models import Item, List
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -32,8 +32,9 @@ class NewListTest(TestCase):
 class ViewListTest(TestCase):
 
     def test_displays_all_items(self):
-        Item.objects.create(text="item1")
-        Item.objects.create(text="item2")
+        list_ = List.objects.create()
+        Item.objects.create(text="item1", list=list_)
+        Item.objects.create(text="item2", list=list_)
 
         response = self.client.get('/lists/the-only-url/')
         self.assertContains(response, "item1")
@@ -44,22 +45,29 @@ class ViewListTest(TestCase):
 
         self.assertTemplateUsed(response, 'view.html')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
    
     def test_creating_items_and_retreiving_it_later(self):
+        list_ = List.objects.create()
         first_item = Item()
         first_item.text = "First item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Second item"
+        second_item.list = list_
         second_item.save()
 
+        self.assertEqual(1, List.objects.count())
         self.assertEqual(2, Item.objects.count())
-        first_saved_item, second_saved_item = Item.objects.all()
+        saved_list = List.objects.first()
+        first_saved_item, second_saved_item = saved_list.item_set.all()
 
         self.assertEqual(first_saved_item.text, 'First item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Second item')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 
--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Item
+from .models import Item, List
 
 # Create your views here.
 def home_page(request):
     return render(request, 'index.html')
 
 def new_list(request):
-    Item.objects.create(text=request.POST.get('item_text', ''))
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-url/')
 
 def view_list(request):
