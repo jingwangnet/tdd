470dda157c35c0e90fb560f8b0b5097c8401a0cc
jing Wang <admin@jingwangnet.com>
2022-02-05 20:22:45 +0800

    Test Add a Token with post email and post email body with url with token.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,21 +1,23 @@
 from django.test import TestCase
 import accounts.views
 from unittest.mock import patch
+from accounts.models import Token
 
 class SendLoginEmailViewTestCase(TestCase):
+    
+    def post_send_login_email(self, follow=False):
+        return self.client.post('/accounts/send_login_email', data={
+           'email': 'edith@example.com'
+        }, follow=True)
+
    
     def test_redirects_to_home_page(self):
-        response = self.client.post('/accounts/send_login_email', data={
-           'email': 'eidthi@example.com'
-        })
+        response = self.post_send_login_email()
         self.assertRedirects(response, '/')
 
     @patch('accounts.views.send_mail')
     def test_sends_mail_to_address_from_post(self, mock_send_mail):
-
-        response = self.client.post('/accounts/send_login_email', data={
-            'email': 'edith@example.com'
-        })
+        self.post_send_login_email()
 
         self.assertEqual(mock_send_mail.called, True)
         (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
@@ -24,16 +26,32 @@ class SendLoginEmailViewTestCase(TestCase):
         self.assertEqual(to_list, ['edith@example.com']) 
 
     def test_adds_success_message(self):
-        response = self.client.post('/accounts/send_login_email', data={
-            'email': 'edith@example.com'
-        }, follow=True)
-
+        response = self.post_send_login_email(follow=True)
         message = list(response.context['messages'])[0]
         self.assertEqual(
             message.message,
             "Check your email, we've sent you a link you can use to log in."
         )
         self.assertEqual(message.tags, 'success')
+
+    def test_creates_token_token_associated_with_email(self):
+        self.post_send_login_email()
+        token = Token.objects.first()
+        self.assertEqual(token.email, 'edith@example.com')
+
+    @patch('accounts.views.send_mail')
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        self.post_send_login_email()
+
+        token = Token.objects.first()
+        expected_url = f'http://testserver/accounts/login?token={token.uid}'
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
+
+         
+        
+
+
 
 class LoginViewTest(TestCase):
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.contrib import messages
+from django.urls import reverse
+from .models import Token
 
 # Create your views here.
 def send_login_email(request):
     email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('accounts:login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists', 
-        'body', 
+        message_body,
         'noreply@superlist', 
         [email]
     )
