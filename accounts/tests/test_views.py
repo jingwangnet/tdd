1aad05c8813f9e426881ae04ed8a23560d03a18c
jing Wang <admin@jingwangnet.com>
2022-02-05 12:29:15 +0800

    Use messages to add info and update base template.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -19,6 +19,18 @@ class SendLoginEmailViewTestCase(TestCase):
 
         self.assertEqual(mock_send_mail.called, True)
         (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
-        self.assertEqual(subject, 'Your login link for Superlist')
+        self.assertEqual(subject, 'Your login link for Superlists')
         self.assertEqual(from_email, 'noreply@superlist')
         self.assertEqual(to_list, ['edith@example.com']) 
+
+    def test_adds_success_message(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        }, follow=True)
+
+        message = list(response.context['messages'])[0]
+        self.assertEqual(
+            message.message,
+            "Check your email, we've sent you a link you can use to log in."
+        )
+        self.assertEqual(message.tags, 'success')
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
+from django.contrib import messages
 
 # Create your views here.
 def send_login_email(request):
     email = request.POST['email']
     send_mail(
-        'Your login link for Superlist', 
+        'Your login link for Superlists', 
         'body', 
         'noreply@superlist', 
         [email]
+    )
+    messages.success(
+       request,
+       "Check your email, we've sent you a link you can use to log in."
     )
     return redirect('/')
